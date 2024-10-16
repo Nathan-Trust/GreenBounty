070f7b59ac01e224e1751e6ca8709ce074620a89
Nathan-Trust <nathantrustekanem@gmail.com>
2024-10-16 16:52:40 +0100

    enhancement:landing page layout

--- src/app/(landing-page)/about/page.tsx
+++ src/app/(landing-page)/about/page.tsx
@@ -1,11 +1,9 @@
 
 const AboutPage = () => {
   return (
-          <div className="w-full screen-max-width pb-20 flex-1">
-          <div className="flex flex-col-reverse md:flex-row justify-between items-center mt-28 border z-20">
-              about us
-      </div>
-    </div>
+      <div>
+          <p>about us</p>
+          </div>
   )
 }
 
