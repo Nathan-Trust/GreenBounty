bf00d5223937d9e1ceda8ba1f504fa4012814cd1
Nathan-Trust <nathantrustekanem@gmail.com>
2024-11-03 07:11:36 +0100

    enhancment:dashboard ui implementation

--- src/app/(dashboard)/dashboard/client.tsx
+++ src/app/(dashboard)/dashboard/client.tsx
@@ -7,9 +7,9 @@ import { useStore } from "@/store/user";
 const DashboardClient = () => {
   const {userData} = useStore()
   return (
-    <div className="grid grid-cols-12 p-6 gap-6">
+    <div className="grid grid-cols-12 p-1.5 mini-md:p-6 max-w-screen-lg mx-auto gap-4 mini-md:gap-6 ">
       {/* top card banner */}
-      <Card className="col-span-12 bg-[#548235] text-white ">
+      <Card className="col-span-12 bg-[#548235] md:order-1  text-white ">
         <CardContent className="flex flex-col-reverse  md:flex-row items-center pb-2 md:pb-0">
           <div className="flex-1">
             <p className="text-2xl font-medium ">
@@ -29,19 +29,19 @@ const DashboardClient = () => {
         </CardContent>
       </Card>
       <BasketFillLoaderCard dataValue={75} />
-      <Card className="col-span-12 lg:col-span-5 border bg-white border-[#548235]  ">
+      <Card className="col-span-12 md:col-span-6 md:order-2 lg:order-3 h-fit lg:col-span-5 border bg-white border-[#548235]  ">
         <CardContent className="flex gap-4 items-start p-4">
           <div className="bg-[#548235] text-white w-12 h-12 flex items-center rounded-md justify-center">
             <CircleDollarSign />
           </div>
           <div>
-            <p>Eco-coin balance</p>
+            <p className="">Eco-Coin</p>
             <p className="text-xl font-medium">${userData?.wallet}</p>
           </div>
         </CardContent>
       </Card>
       <Card
-        className="col-span-12 lg:col-span-5 border border-[#548235] "
+        className="col-span-12 md:col-span-6 lg:col-span-5 md:order-3 h-fit lg:order-4 border border-[#548235] "
         style={{
           backgroundImage:
             "linear-gradient(161deg, #548235 12.61%, #121C0B 105.46%)",
